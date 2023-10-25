b61e722b75d6ebebf0364b19bd225aab969842b7
Alyssa <alyssapausanos1449@gmail.com>
2023-10-25 17:36:25 +1300

    integrate weather recommendation

--- outfitpicker.py
+++ outfitpicker.py
@@ -12,8 +12,6 @@ from keras.datasets import fashion_mnist
 import tensorflow as tf
 import requests
 from datetime import datetime
-
-# Load the weather schema
 from weatherSchema import weatherSchema
 
 # Initialize Q-table
@@ -32,19 +30,70 @@ class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
 
 outfits = {
     'casual': {
-        'top': ['T-shirt/top', 'Pullover', 'Coat'],
-        'bottom': ['Trouser'],
-        'shoes': ['Sneaker', 'Sandal']
+        'Sunny': {
+            'top': ['T-shirt/top', 'Pullover'],
+            'bottom': ['Trouser'],
+            'shoes': ['Sneaker', 'Sandal']
+        },
+        'Rainy': {
+            'top': ['Coat'],
+            'bottom': ['Trouser'],
+            'shoes': ['Sneaker']
+        },
+        'Stormy': {
+            'top': ['Coat', 'Pullover'],
+            'bottom': ['Trouser'],
+            'shoes': ['Sneaker']
+        },
+        'Cloudy': {
+            'top': ['Coat', 'T-shirt/top'],
+            'bottom': ['Trouser'],
+            'shoes': ['Sneaker', 'Sandal']
+        }
     },
     'formal': {
-        'top': ['Shirt', 'Dress'],
-        'bottom': ['Trouser'],
-        'shoes': ['Ankle boot', 'Sandal']
+        'Sunny': {
+            'top': ['Shirt', 'Dress'],
+            'bottom': ['Trouser'],
+            'shoes': ['Ankle boot', 'Sandal']
+        },
+        'Rainy': {
+            'top': ['Coat', 'Shirt', 'Dress'],
+            'bottom': ['Trouser'],
+            'shoes': ['Ankle boot']
+        },
+        'Stormy': {
+            'top': ['Coat'],
+            'bottom': ['Trouser'],
+            'shoes': ['Ankle boot']
+        },
+        'Cloudy': {
+            'top': ['Shirt', 'Dress'],
+            'bottom': ['Trouser'],
+            'shoes': ['Ankle boot', 'Sandal']
+        }
     },
     'sporty': {
-        'top': ['Pullover', 'T-shirt/top'],
-        'bottom': ['Trouser'],
-        'shoes': ['Sneaker']
+        'Sunny': {
+            'top': ['T-shirt/top', 'Pullover'],
+            'bottom': ['Trouser'],
+            'shoes': ['Sneaker']
+        },
+        'Rainy': {
+            'top': ['Coat', 'T-shirt/top'],
+            'bottom': ['Trouser'],
+            'shoes': ['Sneaker']
+        },
+        'Stormy': {
+            'top': ['Coat'],
+            'bottom': ['Trouser'],
+            'shoes': ['Sneaker']
+        },
+        'Cloudy': {
+            'top': ['T-shirt/top', 'Pullover'],
+            'bottom': ['Trouser'],
+            'shoes': ['Sneaker']
+        }
     }
 }
 
@@ -57,34 +106,68 @@ epsilon = 0.2  # exploration rate
 def get_current_state():
     return np.random.randint(0, num_states)
 
-# Initialize state as a global variable
+# Initialize global variables
 state = get_current_state()
+current_weather_code = 0
+weatherCondition = ""
 
-# AI Functions
 
-def display_outfit(outfit_items, class_names, test_images, test_labels):
-    plt.figure(figsize=(5, 5))
-    for i, item in enumerate(outfit_items):
-        outfit_index = np.where(np.array(class_names) == item)[0][0]
-        plt.subplot(1, 3, i + 1)
-        plt.imshow(test_images[np.random.choice(np.where(test_labels == outfit_index)[0])], cmap='gray')
-        plt.title(item)
-        plt.axis('off')
-    plt.show()
+#get current weather condition
+def fetch_current_weather_code():
+    global current_weather_code
+    global weatherCondition
 
-def recommend_outfit(Q, outfits, state, occasion):
-    if occasion in outfits and outfits[occasion]:
-        outfit_options = outfits[occasion]
+    url = "https://api.open-meteo.com/v1/forecast?latitude=-36.8485&longitude=174.7635&hourly=weathercode&forecast_days=1"
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        data = response.json()
+        if 'hourly' in data and 'weathercode' in data['hourly']:
+            weather_codes = data['hourly']['weathercode']
+            current_time = datetime.now()
+            current_hour = current_time.hour
+            if current_hour < len(weather_codes):
+                current_weather_code = weather_codes[current_hour]
+                # Find the dictionary in weatherSchema with matching code
+                for weather_data in weatherSchema:
+                    if weather_data["code"] == current_weather_code:
+                        weatherCondition= weather_data['description']
+                        weather_label.config(text=f"Current Hourly Weather Condition: {weather_data['description']}", font=('Arial', 20, 'bold'), fg='black')
+                        break
+                else:
+                    weather_label.config(text="Weather code not found in the schema.")
+            else:
+                print("Current hour is not within the available range.")
+        else:
+            print("No weather data found in the response.")
     else:
-        raise ValueError("Invalid occasion. Choose from 'casual', 'formal', or 'sporty'.")
+        print(f"Request failed with status code {response.status_code}")
 
-    recommended_outfit = {'top': None, 'bottom': None, 'shoes': None}
+
+
+def recommend_outfit(outfits, occasion, current_weather):
+    """
+    Recommends an outfit based on the current weather condition and the specified occasion.
+    Returns a ist of recommended outfit items based on the specified occasion and current weather condition.
+    
+    Parameters:
+        outfits (dictionary): Dictionary containing outfit options for various occasions and weather conditions.
+        occasion (str): Occasion for which the outfit is being recommended (e.g., 'casual', 'formal', 'sporty').
+        current_weather (str): Current weather condition for which the outfit is being recommended.
+    """
+
+    if occasion in outfits and outfits[occasion] and current_weather in outfits[occasion]:
+        outfit_options = outfits[occasion][current_weather]
+    else:
+        raise ValueError(f"No {occasion} outfit found for the current weather condition.")
+
+    recommended_outfit = []
 
     for item in outfit_options:
         item_options = outfit_options[item]
         if item_options:
             item_index = np.random.randint(len(item_options))
-            recommended_outfit[item] = item_options[item_index]
+            recommended_outfit.append(item_options[item_index])
         else:
             return {"error": f"No {item} outfit found for the current occasion."}
 
@@ -95,25 +178,20 @@ def recommend_outfit(Q, outfits, state, occasion):
 root = tk.Tk()
 root.title("Outfit Recommendation System")
 root.geometry("600x500")
-
-# Configure the style
 root.configure(bg='pink')
-
-# Create a top panel for weather information
 weather_panel = tk.Frame(root, bg='pink')
 weather_panel.pack(side="top", fill="both", expand=True)
-
-# Create another panel for outfit display
 outfit_panel = tk.Frame(root, bg='pink')
 outfit_panel.pack(side="left", fill="both", expand=True)
 
+# Display the outfit in the UI
 def display_outfit_ui(outfit_items, class_names, test_images, test_labels, panel, occasion):
     for widget in panel.winfo_children():
         widget.destroy()
 
     if occasion == "formal" and "Dress" in outfit_items:
         label_names = ["Dress", "Shoes"]
-        outfit_items = [outfit_items[0], outfit_items[2]]  # Fetch Dress and Shoes only
+        outfit_items = [outfit_items[0], outfit_items[2]]
     else:
         label_names = ["Top", "Bottom", "Shoes"]
 
@@ -142,10 +220,17 @@ def dislike_outfit():
     handle_feedback(-1)
 
 def handle_feedback(feedback):
+    """
+    Handles the feedback provided for the recommended outfit and updates the Q-table accordingly.
+    The function also triggers the display of a new outfit recommendation based on the updated state.
+
+    Parameter:
+        feedback (int):  A positive value indicates a like, while a negative value indicates a dislike.
+    """
     global state  # Use the global state variable
 
     # Update Q-table based on the feedback
-    old_value = Q[state, 0]  # Assuming the action is 0 (uniformly random in this case)
+    old_value = Q[state, 0]
     next_max = np.max(Q[state])
     new_value = (1 - alpha) * old_value + alpha * (feedback * 10 + gamma * next_max)
     Q[state, 0] = new_value
@@ -154,13 +239,14 @@ def handle_feedback(feedback):
     state = get_current_state()
 
     chosen_occasion = selected_occasion.get()
-    recommended_outfit = recommend_outfit(Q, outfits, state, chosen_occasion)
+    recommended_outfit = recommend_outfit(outfits,chosen_occasion, weatherCondition)
     if 'error' in recommended_outfit:
-        print(recommended_outfit['error'])
+        print("error in handle_feedback " +recommended_outfit['error'])
     else:
-        display_outfit_ui(list(recommended_outfit.values()), class_names, test_images, test_labels, outfit_panel, chosen_occasion)
+        display_outfit_ui(recommended_outfit, class_names, test_images, test_labels, outfit_panel, chosen_occasion)
 
 
+# Add like and dislike buttons and occasion dropdown on the UI
 occasions = ['casual', 'formal', 'sporty']  # List of occasions
 selected_occasion = tk.StringVar(root)
 selected_occasion.set(occasions[0])  # Set the default occasion
@@ -177,64 +263,39 @@ dislike_button = tk.Button(control_panel, text="Dislike", command=dislike_outfit
 dislike_button.config(bg='pink')
 dislike_button.pack(side="top", padx=10, pady=10)
 
-# Configure the style for the occasion dropdown
 occasion_dropdown = tk.OptionMenu(control_panel, selected_occasion, *occasions)
 occasion_dropdown.config(bg='pink', relief="flat")
 occasion_dropdown.pack(side="top", padx=10, pady=10)
 
 
 def update_occasion(*args):
+    """
+    Updates the selected occasion and triggers the recommendation of a new outfit based on the chosen occasion
+    and the current weather condition.
+    """
     chosen_occasion = selected_occasion.get()
-    recommended_outfit = recommend_outfit(Q, outfits, state, chosen_occasion)
-    if 'error' in recommended_outfit:
-        print(recommended_outfit['error'])
-    else:
-        display_outfit_ui(list(recommended_outfit.values()), class_names, test_images, test_labels, outfit_panel, chosen_occasion)
-
+    current_weather = weatherCondition
+    try:
+        recommended_outfit = recommend_outfit(outfits, chosen_occasion, current_weather)
+        display_outfit_ui(recommended_outfit, class_names, test_images, test_labels, outfit_panel, chosen_occasion)
+    except ValueError as e:
+        print(e)
 
 selected_occasion.trace("w", update_occasion)
-
-url = "https://api.open-meteo.com/v1/forecast?latitude=-36.8485&longitude=174.7635&hourly=weathercode&forecast_days=1"
-response = requests.get(url)
 
 weather_label = tk.Label(weather_panel, text="Weather code: ", bg='pink')
 weather_label.pack(side="top", padx=10, pady=10)
 
-current_weather_code = 0
 
-# Update the label's text with the current weather code
-if response.status_code == 200:
-    data = response.json()
-    if 'hourly' in data and 'weathercode' in data['hourly']:
-        weather_codes = data['hourly']['weathercode']
-        current_time = datetime.now()
-        current_hour = current_time.hour
-        if current_hour < len(weather_codes):
-            current_weather_code = weather_codes[current_hour]
-            # Find the dictionary in weatherSchema with matching code
-            for weather_data in weatherSchema:
-                if weather_data["code"] == current_weather_code:
-                    print(current_weather_code)
-                    weather_label.config(text=f"Current Hourly Weather Condition: {weather_data['description']}", font=('Arial', 20, 'bold'), fg='black')
-                    break
-            else:
-                weather_label.config(text="Weather code not found in the schema.")
-        else:
-            weather_label.config(text="Current hour is not within the available range.")
-    else:
-        weather_label.config(text="No weather data found in the response.")
-else:
-    weather_label.config(text=f"Request failed with status code {response.status_code}")
-
-
-# Run the UI
 if __name__ == '__main__':
     # Call functions to display initial outfit images
     chosen_occasion = selected_occasion.get()
-    recommended_outfit = recommend_outfit(Q, outfits, state, chosen_occasion)
-    if 'error' in recommended_outfit:
-        print(recommended_outfit['error'])
-    else:
-        display_outfit_ui(list(recommended_outfit.values()), class_names, test_images, test_labels, outfit_panel, chosen_occasion)
+    fetch_current_weather_code()
+    current_weather = weatherCondition
+    try:
+        recommended_outfit = recommend_outfit(outfits, chosen_occasion, current_weather)
+        display_outfit_ui(recommended_outfit, class_names, test_images, test_labels, outfit_panel, chosen_occasion)
+    except ValueError as e:
+        print(e)
 
     root.mainloop()
